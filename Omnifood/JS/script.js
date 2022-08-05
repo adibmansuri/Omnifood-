62dc0c72d16556893459a6f634a21c3b53247e2c
Adib <mansuri.adib96@gmail.com>
2022-08-05 18:57:16 +0200

    first

--- Omnifood/JS/script.js
+++ Omnifood/JS/script.js
@@ -43,24 +43,9 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+//sticky nav
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
-
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
-
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+///////////////////////////////////////////////////////////
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
